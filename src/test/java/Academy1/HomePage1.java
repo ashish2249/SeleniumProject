6303a97af8b4ac9c40bf6da99d8d8332589d92c1
ashish2249 <ash252249@gmail.com>
2020-08-30 23:47:16 +0530

    added loginpage for email & pswd

--- src/test/java/Academy1/HomePage1.java
+++ src/test/java/Academy1/HomePage1.java
@@ -3,6 +3,7 @@ package Academy1;
 import java.io.IOException;
 import org.testng.annotations.Test;
 
+import pageObject.LoginPage;
 import pageObject.landingPage;
 import resources.base1;
 
@@ -16,8 +17,13 @@ public class HomePage1 extends base1{
 	    driver.get("http://www.qaclickacademy.com/");
 	    
 	    landingPage l = new landingPage(driver);
-	    
 	    l.getLogin().click();
+	    
+	    LoginPage lp = new LoginPage(driver);
+	    lp.getEmail().sendKeys("ashish");
+	    lp.getPassword().sendKeys("1234");
+	    
+	    
 	    
 	    
 	}
